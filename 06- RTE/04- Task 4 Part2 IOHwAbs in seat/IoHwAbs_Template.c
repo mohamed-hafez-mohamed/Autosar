61ad9d819f4a2c976ab173a17964ee267b481d54
Mohamed Hafez Mohamed <moh.hafez98@gmail.com>
2021-08-25 23:09:27 +0200

    Task4 Part2 Added IoHwAbs

---
+++ 06- RTE/04- Task 4 Part2 IOHwAbs in seat/IoHwAbs_Template.c
@@ -0,0 +1,240 @@
+/**
+ *
+ * \file IoHwAbs_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: IoHwAbs
+ *
+ * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
+ * Generated on 8/25/2021 07:13 م
+ *
+ * For any inquiries: hassan.m.farahat@gmail.com
+ *
+ */
+
+#include "Rte_IoHwAbs.h"
+
+/* For reading from Position Sensors */
+#include "Adc.h"
+
+/* For reading from Weight Sesnor */
+#include "Spi.h"
+
+/* For driving Motors (height, slide, incline) */
+#include "Dio.h"
+
+/* Internal variables to read Position sensors in one Adc Group */
+static Adc_ValueGroupType IoHwAb_PositionSensorsReadings[ADC_GR0_NUM_CHANNELS] =
+{
+	/* Height */
+	0,
+	/* Incline */
+	0,
+	/* Slide */
+	0
+};
+
+/**
+ *
+ * Runnable IoHwAbs_ECUGet_Height
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppECUGetSensorPosition_IoGetHeight
+ *
+ */
+
+void IoHwAbs_ECUGet_Height(IopositionSensorReadingType *Position)
+{
+	Std_ReturnType status;
+
+	status = Adc_ReadGroup(AdcConf_AdcGroup_PositionsSensorsGrp, IoHwAb_PositionSensorsReadings);
+
+	if (status == E_OK)
+	{
+		/* IoPositionSensorReadingType has to be adjusted in arxml file */
+		/* as per Adc resolution "Adc_ValueGroupType can be uint8 or uint16" */
+		*position = (IoPositionSensorReadingType)IoHwAb_PositionSensorsReadings[0];
+	}
+	else
+	{
+		/* Operation shall be updated to return failure error code */
+	}
+}
+
+/**
+ *
+ * Runnable IoHwAbs_ECUGet_Incline
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppECUGetSensorPosition_IoGetIncline
+ *
+ */
+
+void IoHwAbs_ECUGet_Incline(IopositionSensorReadingType *Position)
+{
+	Std_ReturnType status;
+
+	status = Adc_ReadGroup(AdcConf_AdcGroup_PositionsSensorsGrp, IoHwAb_PositionSensorsReadings);
+
+	if (status == E_OK)
+	{
+		/* IoPositionSensorReadingType has to be adjusted in arxml file */
+		/* as per Adc resolution "Adc_ValueGroupType can be uint8 or uint16" */
+		*position = (IoPositionSensorReadingType)IoHwAb_PositionSensorsReadings[1];
+	}
+	else
+	{
+		/* Operation shall be updated to return failure error code */
+	}
+}
+
+/**
+ *
+ * Runnable IoHwAbs_ECUGet_Slide
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppECUGetSensorPosition_IoGetSlide
+ *
+ */
+
+void IoHwAbs_ECUGet_Slide(IopositionSensorReadingType *Position)
+{
+	Std_ReturnType status;
+
+	status = Adc_ReadGroup(AdcConf_AdcGroup_PositionsSensorsGrp, IoHwAb_PositionSensorsReadings);
+
+	if (status == E_OK)
+	{
+		/* IoPositionSensorReadingType has to be adjusted in arxml file */
+		/* as per Adc resolution "Adc_ValueGroupType can be uint8 or uint16" */
+		*position = (IoPositionSensorReadingType)IoHwAb_PositionSensorsReadings[2];
+	}
+	else
+	{
+		/* Operation shall be updated to return failure error code */
+	}
+}
+
+/**
+ *
+ * Runnable IoHwAbs_ECUGet_Weight
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppECUGetWeightPosition_IoGetWeight
+ *
+ */
+
+void IoHwAbs_ECUGet_Weight(IoWeightSensorReadingType *weight)
+{
+	Std_ReturnType status;
+
+	Spi_DataType spiData;
+
+	status = Spi_ReadIB(SpiConf_SpiChannel_WeightSensor, &spiData);
+
+	if (status == E_OK)
+	{
+		/* IoWeightSensorReadingType has to be adjusted in arxml file */
+		/* as per Spi_DataType "can be uint8 or uint16" */
+		*weight = (IoWeightSensorReadingType)spiData;
+	}
+	else
+	{
+		/* Operation shall be updated to return failure error code */
+	}
+}
+
+/**
+ *
+ * Runnable IoHwAbs_ECUGet_SetReverse_Height
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppECUSetMotor_IoSetHeightReverse
+ *
+ */
+
+void IoHwAbs_ECUGet_SetReverse_Height(void)
+{
+	Std_ReturnType status;
+
+	/* Write LOW to move reverse */
+	Dio_WriteChannel(DioConfg_DioChannel_HeightMotorCh, STD_LOW);
+}
+
+/**
+ *
+ * Runnable IoHwAbs_ECUGet_SetReverse_Incline
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppECUSetMotor_IoSetInclineReverse
+ *
+ */
+
+void IoHwAbs_ECUGet_SetReverse_Incline(void)
+{
+	Std_ReturnType status;
+	/* Write LOW to move reverse */
+	Dio_WriteChannel(DioConfg_DioChannel_InclineMotorCh, STD_LOW);
+}
+
+/**
+ *
+ * Runnable IoHwAbs_ECUGet_SetReverse_Slide
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppECUSetMotor_IoSetSlideReverse
+ *
+ */
+
+void IoHwAbs_ECUGet_SetReverse_Slide(void)
+{
+	Std_ReturnType status;
+	/* Write LOW to move reverse */
+	Dio_WriteChannel(DioConfg_DioChannel_SlideMotorCh, STD_LOW);
+}
+
+/**
+ *
+ * Runnable IoHwAbs_ECUGet_Setforward_Height
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppECUSetMotor_IoSetHeightForward
+ *
+ */
+
+void IoHwAbs_ECUGet_Setforward_Height(void)
+{
+	Std_ReturnType status;
+	/* Write HIGH to move forward */
+	Dio_WriteChannel(DioConfg_DioChannel_HeightMotorCh, STD_HIGH);
+}
+
+/**
+ *
+ * Runnable IoHwAbs_ECUGet_Setforward_Incline
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppECUSetMotor_IoSetInclineForward
+ *
+ */
+
+void IoHwAbs_ECUGet_Setforward_Incline(void)
+{
+	Std_ReturnType status;
+	/* Write HIGH to move forward */
+	Dio_WriteChannel(DioConfg_DioChannel_InclineMotorCh, STD_HIGH);
+}
+
+/**
+ *
+ * Runnable IoHwAbs_ECUGet_Setforward_Slide
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppECUSetMotor_IoSetSlideForward
+ *
+ */
+
+void IoHwAbs_ECUGet_Setforward_Slide(void)
+{
+	Std_ReturnType status;
+	/* Write HIGH to move forward */
+	Dio_WriteChannel(DioConfg_DioChannel_SlideMotorCh, STD_HIGH);
+}
---
+++ 06- RTE/04- Task 4 Part2 IOHwAbs in seat/SlideSensor_Template.c
@@ -0,0 +1,50 @@
+/**
+ *
+ * \file SlideSensor_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: SlideSensor
+ *
+ * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
+ * Generated on 8/25/2021 07:14 م
+ *
+ * For any inquiries: hassan.m.farahat@gmail.com
+ *
+ */
+
+#include "Rte_SlideSensor.h"
+
+
+/**
+ *
+ * Runnable SlideSensor_GetPosition
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppSlideSensor_GetPosition
+ *
+ */
+
+void SlideSensor_GetPosition (SensorPositionType* Position)
+{
+	Std_ReturnType              status;
+	IoPositionSensorReadingType position;
+
+	/* Server Call Points */
+	status = Rte_Call_rpIOGetSlide_IOGet(&position);
+	if(position == 0)
+	{
+      *Position = SENSOR_POSITION_STEP_0;
+	}
+	else if(position > 0 && position <= 64)
+	{
+		*Position = SENSOR_POSITION_STEP_1;
+	}
+	else if(position > 64 && position <= 192)
+	{
+		*Position = SENSOR_POSITION_STEP_2;
+	}
+	else
+	{
+		*Position = SENSOR_POSITION_STEP_3;
+	}
+	
+}
+
